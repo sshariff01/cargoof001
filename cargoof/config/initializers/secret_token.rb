197e48b13ddfc61b80c9c4ce5a063c47de323e1f
sshariff01 <shoabe.shariff@gmail.com>
2013-11-11 20:57:04 -0500

    Create new rails project. Configure with mysql db

---
+++ cargoof/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cargoof::Application.config.secret_key_base = '149895477846777f9a48c8a411418c4549b117ed20877413de3b0ed4f1a8e7c5421f018945d864fd62eb6f9cde816630e6e82297adfcf3b5c3cfb56984350bce'
