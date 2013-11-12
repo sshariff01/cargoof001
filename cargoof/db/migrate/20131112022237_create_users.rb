380a6e38f987c087354e26f57550fd07cc9c02fa
sshariff01 <shoabe.shariff@gmail.com>
2013-11-11 21:35:55 -0500

    Fill and migrate User model

--- cargoof/db/migrate/20131112022237_create_users.rb
+++ cargoof/db/migrate/20131112022237_create_users.rb
@@ -1,7 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-
+      t.string "first_name", :null => false
+      t.string "last_name", :null => false
+      t.string "username"
+      t.string "hashed_password", :null => false
+      t.string "email", :null => false
+      
       t.timestamps
     end
   end
